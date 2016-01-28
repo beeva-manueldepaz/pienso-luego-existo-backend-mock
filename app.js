63bedfcbcd5085554331ba26022b1ea8c67a8ffc
beeva-joseGutierrez <jose.gutierrez@beeva.com>
2016-01-28 16:09:16 +0100

    CORS

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var port = normalizePort(process.env.PORT || '3000');
 var app = express();
 	app.set('port', port);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var piensos = require('./piensos');
 
 app.use('/piensos',piensos);
