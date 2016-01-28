f1ab93538d873a2f7f5cd9c4aea4eceae40b7fd8
beeva-joseGutierrez <jose.gutierrez@beeva.com>
2016-01-28 16:03:10 +0100

    Mockup models

--- piensos/index.js
+++ piensos/index.js
@@ -3,11 +3,46 @@ var router = express.Router();
 
 /* GET piensos listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+  var piensos = [
+    {
+      id: 1,
+      marca: "Purina",
+      modelo: "Junior Pro",
+      peso: 2.5,
+      precio: 5.99,
+      descripcion: "Indicado para perros jovenes",
+      fCaducidad: "1453912360",
+      logo: "sdfasdfasf"
+    },
+    {
+      id: 2,
+      marca: "acANA",
+      modelo: "Junior Pro",
+      peso: 2.5,
+      precio: 5.99,
+      descripcion: "Indicado para perros jovenes",
+      fCaducidad: "1453912360",
+      logo: "sdfasdfasf"
+    }
+  ];
+
+  res.status(200).send(piensos);
+
 });
 
 router.get('/:id', function(req, res, next) {
-  res.send({id: req.params.id });
+  res.send({
+    id: 1,
+    marca: "Purina",
+    modelo: "Junior Pro",
+    peso: 2.5,
+    precio: 5.99,
+    descripcion: "Indicado para perros jovenes",
+    fCaducidad: "1453912360",
+    logo: "sdfasdfasf"
+  });
 });
 
 module.exports = router;
