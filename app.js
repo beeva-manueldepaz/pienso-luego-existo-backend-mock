377fbfa0cb9e4de2ae5e237d8c615f6c770a3830
Fernando <fernando.canto@beeva.com>
2016-01-28 13:09:28 +0100

    First Express Mock Server

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = normalizePort(process.env.PORT || '3000');
+var app = express();
+	app.set('port', port);
+
+var piensos = require('./piensos');
+
+app.use('/piensos',piensos);
+
+app.get('/', function(req, res, next) {
+  res.status(200).send({error: 'You should not be here!'});
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+function normalizePort(val) {
+  
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) { return val;  }
+  if (port >= 0) { return port;  }
+
+  return false;
+}
