c1aebf6b93afd387d3aa6d423f0aa93020269634
Manuel Eusebio de Paz Carmona <manuel.depaz@beeva.com>
2016-01-29 09:13:46 +0100

    New Libraries - jmespath

--- piensos/index.js
+++ piensos/index.js
@@ -9,7 +9,7 @@ var piensos = [
 		{ pienso: { id: 2, marca: "Akana", modelo: "Junior Pro", peso: 2.5, precio: 5.99, descripcion: "Indicado para perros jovenes", fCaducidad: "1453912360", logo: "sdfasdfasf" } },
 		{ pienso: { id: 3, marca: "Firskies", modelo: "Perros Minor", peso: 300, precio: 60, descripcion: "Friskies, para perros canijos", fCaducidad: 1453999136 } },
 		{ pienso: { id: 4, marca: "Firskies", modelo: "Perros grandes", peso: 600, precio: 49.99, descripcion: "Friskies, para perros de verdad", fCaducidad: 1485535136 } },
-		{ pienso: { id:5, marca: "McPerris", modelo: "Plus", peso: 1000, precio: 35.99, descripcion: "Para los más perros", fCaducidad: 1456591136 } },
+		{ pienso: { id: 5, marca: "McPerris", modelo: "Plus", peso: 1000, precio: 35.99, descripcion: "Para los más perros", fCaducidad: 1456591136 } },
 		{ pienso: { id: 6, marca: "Doggie", modelo: "Adulto", peso: 100, precio: 43.80, descripcion: "Para perrear", fCaducidad: 1454517536 } }
 	];
 
@@ -21,6 +21,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id', function(req, res, next) {
 
+	// With jmespath TODO
+	//jmespath.search(data, "data[?pienso.id=`1`]")
+	//jmespath.search(data, "data.data[?id=`1`]")
+
+	// With lodash;
 	var id = parseInt(req.params.id);
 	var reply = _.find(piensos, {pienso:{id:id}}) || { pienso: {}} ;
 
